0675fd3d7c2b8e9b1766d0e35c776f0451faed29
Dev03 <desenvolvimento03@doiscomunicacao.com.br>
2022-09-15 16:57:04 -0300

    44min aula stage 2

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './styles/main.css';
+import logoImg from './assets/logo-nlw-esports.svg';
+
+function App() {
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} />
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+          <a href="">
+            <img src="/game-1.png" alt="game-1" />
+          </a>
+          <a href="">
+            <img src="/game-2.png" alt="game-2" />
+          </a>
+          <a href="">
+            <img src="/game-3.png" alt="game-3" />
+          </a>
+          <a href="">
+            <img src="/game-4.png" alt="game-4" />
+          </a>
+          <a href="">
+            <img src="/game-5.png" alt="game-5" />
+          </a>
+          <a href="">
+            <img src="/game-6.png" alt="game-6" />
+          </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
