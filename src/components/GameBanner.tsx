27703dd507a7b063f4125af11a5b8201f0033856
Dev03 <desenvolvimento03@doiscomunicacao.com.br>
2022-09-16 14:23:15 -0300

    adicionado a listagem de games

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -1,7 +1,7 @@
 interface GameBannerProps {
     bannerUrl: string;
     title: string;
-    adsCount: string;
+    adsCount: number;
 }
 
 export function GameBanner (props: GameBannerProps){
@@ -10,7 +10,7 @@ export function GameBanner (props: GameBannerProps){
             <img src={props.bannerUrl} alt="game-1" />
             <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0'>
                 <strong className='font-bold text-white block'>{props.title}</strong>
-                <span className='text-zinc-300 text-sm block'>{props.adsCount} anúncios</span>
+                <span className='text-zinc-300 text-sm block'>{props.adsCount} anúncio(s)</span>
             </div>
         </a>
     );
