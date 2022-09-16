4172e58a183da6cd575e4ef05647b7292338a840
Dev03 <desenvolvimento03@doiscomunicacao.com.br>
2022-09-16 17:33:21 -0300

    alterações até 31min aula stage 5

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, FormEvent } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import * as Checkbox from '@radix-ui/react-checkbox';
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
@@ -19,16 +19,24 @@ export function CreateAdModal () {
         fetch('http://localhost:3333/games').then(response => response.json()).then(data => setGames(data));
     }, []);
 
+    function handleCreateAd(event: FormEvent) {
+        event.preventDefault();
+        const formData = new FormData(event.target as HTMLFormElement);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+    }
+
     return (
         <Dialog.Portal>
               <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
 
               <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
                 <Dialog.Title className='text-3xl font-black'>Publique um anúcio</Dialog.Title>
-                <form className='mt-8 flex flex-col gap-4'>
+                <form onSubmit={handleCreateAd} className='mt-8 flex flex-col gap-4'>
                     <div className='flex flex-col gap-2'>
                       <label htmlFor="game" className='font-semibold'>Qual o game?</label>
-                      <select 
+                      <select
+                        name='game'
                         id='game'
                         className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
                         defaultValue=""
@@ -41,17 +49,17 @@ export function CreateAdModal () {
                     </div>
                     <div className='flex flex-col gap-2'>
                       <label htmlFor="name">Seu nome (ou nickname)</label>
-                      <Input id='name' placeholder='Como et chamam dentro do game?' />
+                      <Input name='name' id='name' placeholder='Como et chamam dentro do game?' />
                     </div>
 
                     <div className='grid grid-cols-2 gap-6'>
                       <div className='flex flex-col gap-2'>
                         <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-                        <Input type="number" id='yearsPlaying' placeholder='Tudo bem ser ZERO' />
+                        <Input type="number" name='yearsPlaying' id='yearsPlaying' placeholder='Tudo bem ser ZERO' />
                       </div>
                       <div className='flex flex-col gap-2'>
                         <label htmlFor="discord">Qual seu discord?</label>
-                        <Input id='discord' placeholder='usuário#0000' />
+                        <Input id='discord' name='discord' placeholder='usuário#0000' />
                       </div>
                     </div>
 
